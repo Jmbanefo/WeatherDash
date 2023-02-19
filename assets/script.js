be2c7d26de5082b1ac9c85bcbf3dd925c743f8d6
Justin <mbanefojustin@gmail.com>
2023-02-18 18:22:15 -0600

    add user input

--- assets/script.js
+++ assets/script.js
@@ -1,13 +1,16 @@
 let APIKey = 'b23a3c309d3cc6ecad909e4ffc9c20d4'
-let city = ''; 
-var currentDate = dayjs().format('MMM D YYYY');
 
+var currentDate = dayjs().format('MMM D YYYY');
 $("#currentDay").html(currentDate);
+
+let citysearch = document.getElementById("mycity"); 
+
+
 
 
 // let cityInput = document.getElementById('mycity')
 
-let queryURL = "http://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + APIKey;
+let queryURL = "http://api.openweathermap.org/data/2.5/weather?q=" + citysearch + "&appid=" + APIKey;
 
 //Need to have the user input working
 fetch(queryURL)
