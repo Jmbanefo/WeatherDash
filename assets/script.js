5fbaef7f04ae279e66fe462f868b82172da6db6d
Justin <mbanefojustin@gmail.com>
2023-02-19 12:21:57 -0600

    change input value

--- assets/script.js
+++ assets/script.js
@@ -3,13 +3,26 @@ let APIKey = 'b23a3c309d3cc6ecad909e4ffc9c20d4'
 var currentDate = dayjs().format('MMM D YYYY');
 $("#currentDay").html(currentDate);
 
-let citysearch = document.getElementById("mycity"); 
+let citysearch = document.getElementById("mycity").value; 
+let currentCity = '';
+let searchBtn = $('#cityInput');
 
 
-let queryURL = "http://api.openweathermap.org/data/2.5/weather?q=" + citysearch + "&appid=" + APIKey;
+let queryURL = "http://api.openweathermap.org/data/2.5/weather?q=" + currentCity + "&appid=" + APIKey;
 
 
-fetch(queryURL)
+
+    
+
+
+searchBtn.on("click", citySubmit)
+
+function citySubmit (event) {
+    event.preventDefault();
+    currentCity = citysearch;
+    console.log(currentCity);
+
+    fetch(queryURL)
     .then(function (response) {
         return response.json();
     })
@@ -17,4 +30,5 @@ fetch(queryURL)
         console.log('Let me see some data \n----------');
         console.log(data);
       });
-    
+    return;
+}
